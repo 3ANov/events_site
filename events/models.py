340b3259d36a96836b13b3cf3ef1e836bfea8589
3ANov <3anovdev@gmail.com>
2020-10-18 00:34:43 +0300

    Форматирую модели - надо продолжить

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class EventInstanse(models.Model):
+    name = models.CharField(max_length=200)
+    time = models.DateTimeField()
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    event_instanse = models.ForeignKey()
