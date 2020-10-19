94fa14e0115cc65046f047ac0f17ccd7844aed44
3ANov <3anovdev@gmail.com>
2020-10-19 22:30:47 +0300

    Небольшая правка urls.py

--- events_site/urls.py
+++ events_site/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('events.urls'))
+    path('api/', include('events.urls')),
+    #path('', include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
