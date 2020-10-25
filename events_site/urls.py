8cfc835b52e0025f06ea8f433be627ebcb831154
3ANov <3anovdev@gmail.com>
2020-10-25 17:27:01 +0300

    Добавляю vue.js

--- events_site/urls.py
+++ events_site/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('events.urls')),
-    #path('', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
