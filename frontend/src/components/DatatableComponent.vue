11dc52c63e0e1dc0b46dec8be431c0b6ba5ab522
3ANov <3anovdev@gmail.com>
2020-10-27 15:43:03 +0300

    Пилим фронтенд

---
+++ frontend/src/components/DatatableComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="">
+    <h1 style="text-align: center;">Datatable with 3rd Party API</h1>
+     <blog-post
+      v-for="event in results"
+      v-bind:key="post.id"
+      v-bind:post="post"
+    ></blog-post>
+  </div>
+</template>
+<style scoped></style>
+<script>
+import axios from "axios";
+export default {
+  name: "DatatableComponent",
+  data() {
+    return {
+      page: 1,
+      totalEvents: 0,
+      numberOfPages: 0,
+      results: [],
+      loading: true,
+      options: {},
+      headers: [
+        { text: "Passenger Name", value: "name" },
+        { text: "Number Of Trips", value: "trips" },
+        { text: "Airline", value: "airline[0].name" },
+        { text: "Logo", value: "logo" },
+        { text: "Website", value: "website" },
+      ],
+    };
+  },
+  watch: {
+    options: {
+      handler() {
+        this.readDataFromAPI();
+      },
+    },
+    deep: true,
+  },
+  methods: {
+    readDataFromAPI() {
+      this.loading = true;
+      console.log("Page Number ", this.page);
+      axios
+        .get(
+          "http://127.0.0.1:8000/api/events/" +
+            "?page=" +
+            this.page
+        )
+        .then((response) => {
+          this.loading = false;
+          this.results = response.data.results;
+          this.totalEvents = response.data.count;
+        });
+    },
+  },
+  mounted() {
+    this.readDataFromAPI();
+  },
+};
+</script>
