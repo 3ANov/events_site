91914b9b01d5404b8bc4803134711e687eb8b2ba
3ANov <3anovdev@gmail.com>
2020-10-28 12:25:11 +0300

    Пилим фронтенд

--- frontend/src/components/DatatableComponent.vue
+++ frontend/src/components/DatatableComponent.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <h1 class="text-center">Список событий</h1>
 
-      <div class="d-flex justify-content-center">
-        <b-button variant="primary" class="m-1" v-bind:href="previous">Previous</b-button>
-        <b-button variant="primary" class="m-1" v-bind:href="next">Next</b-button>
-      </div>
+    <div class="d-flex justify-content-center">
+      <b-button variant="primary" class="m-1" v-on:click="click_previous">Previous</b-button>
+      <b-button variant="primary" class="m-1" v-on:click="click_next">Next</b-button>
+    </div>
     <ul id="example-1">
       <li v-for="event in results" v-bind:key="event.id">
         {{ event.title }}
@@ -23,6 +23,7 @@ export default {
   data() {
     return {
       page: 1,
+      current_url: "http://127.0.0.1:8000/api/events/?page=1",
       next: null,
       previous: null,
       totalEvents: 0,
@@ -33,21 +34,19 @@ export default {
   watch: {
     options: {
       handler() {
-        this.readDataFromAPI();
+        this.readDataFromAPI(this.current_url);
       },
     },
     deep: true,
   },
   methods: {
-    readDataFromAPI() {
+    readDataFromAPI(url) {
       this.loading = true;
       console.log("Page Number ", this.page);
+      console.log("Current url ", url);
+      console.log(this.page);
       axios
-          .get(
-              "http://127.0.0.1:8000/api/events/" +
-              "?page=" +
-              this.page
-          )
+          .get(url)
           .then((response) => {
             this.loading = false;
             this.next = response.data.next;
@@ -56,9 +55,21 @@ export default {
             this.totalEvents = response.data.count;
           });
     },
+    click_next: function () {
+      if (this.next != null){
+        this.readDataFromAPI(this.next)
+      }
+    },
+    click_previous: function () {
+      if(this.previous != null){
+        this.readDataFromAPI(this.previous)
+      }
+    }
   },
   mounted() {
-    this.readDataFromAPI();
+    this.readDataFromAPI(this.current_url);
   },
+
 };
+
 </script>
